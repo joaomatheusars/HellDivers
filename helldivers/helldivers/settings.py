060b5611edf5e7c397561f904fff571b2022a79d
João Matheus <joaomatheusars@gmail.com>
2024-07-29 22:49:50 -0300

    fix: config db

--- helldivers/helldivers/settings.py
+++ helldivers/helldivers/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 TAILWIND_APP_NAME = 'theme'
 
 INTERNAL_IPS = [
+    '*',
     "127.0.0.1",
 ]
 ###
